7c562642e66b02f76d324bd3b1ee368eb902c4ef
Poojitha-0508 <attilipooja@gmail.com>
2025-08-16 21:08:36 +0530

    Update BinarySearch_Ascending.java

--- 01_Searching/BinarySearch/BinarySearch_Ascending.java
+++ 01_Searching/BinarySearch/BinarySearch_Ascending.java
@@ -17,7 +17,7 @@ public class Main
 	static int BinarySearch(int[] arr,int target){
 		int start=0,end=arr.length-1;
 		while(start<=end){
-			int mid=(start+end)/2;
+			int mid=(start+(end-start))/2;
 			if(arr[mid]==target){
 				return mid;
 			}
