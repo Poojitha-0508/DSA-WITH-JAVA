e34969b2fcd8783c9720415f4a49b80dbdbe1bfa
Poojitha-0508 <attilipooja@gmail.com>
2025-08-16 22:05:22 +0530

    Update SearchInRotatedSortedArray.java

--- 01_Searching/BinarySearch/Problems/SearchInRotatedSortedArray.java
+++ 01_Searching/BinarySearch/Problems/SearchInRotatedSortedArray.java
@@ -12,42 +12,37 @@ public class Main
 	        	arr[i]=in.nextInt();
 		}
 		int key=in.nextInt();
-		int index;
 	    int pivot=findPivot(arr);
-	    if (pivot==-1){
-	        index=BinarySearch (arr, key, 0 ,n-1);
-	    }else if(arr[pivot]==key){
-	        index=pivot;
-	    }else{
-    	    index=BinarySearch (arr, key, 0,pivot);
-    	    if (index==-1){
-    	        index=BinarySearch (arr, key, pivot ,n-1);
-    	    }
+	    int index=BinarySearch (arr, key, 0,pivot);
+	    if (index==-1){
+	        index=BinarySearch (arr, key, pivot+1 ,arr.length-1);
 	    }
-	    System.out.println (index);
+	    System.out.println(index);
 	    
 	}
 	static int findPivot(int[] arr){
 		int start=0,end=arr.length-1;
+		int mid;
 		while(start<=end){
-			int mid=(start+(end-start))/2;
-		    if (mid<end && arr[mid]>arr[mid+1]){
-		        return mid;
-		    }
-		    else if(mid>start && arr[mid]<arr[mid-1]){
-		        return mid-1;
-		    }
-		    else if(arr[start]<arr[mid]){
-		        start=mid+1;
-		    }else{
-		        end=mid-1;
-		    }
+			mid=(start+end)/2;
+			if(mid<end && arr[mid]>arr[mid+1]){
+				return mid;
+			}
+			else if(mid>start && arr[mid]<arr[mid-1]){
+				return mid-1;
+			}
+			else if (arr[mid]<arr[start]){
+				end=mid-1;
+			}
+			else{
+				start=mid+1;
+			}
 		}
 		return -1;
 	}
 	static int BinarySearch(int[] arr,int key,int start,int end){
 		while(start<=end){
-			int mid=(start+(end-start))/2;
+			int mid=(start+end)/2;
 			if(arr[mid]==key){
 				return mid;
 			}
