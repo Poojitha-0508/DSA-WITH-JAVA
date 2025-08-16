a329092770671088c381099dddf76db2759d5bd4
Poojitha-0508 <attilipooja@gmail.com>
2025-08-16 21:58:37 +0530

    Update SplitArray.java

--- 01_Searching/BinarySearch/Problems/SplitArray.java
+++ 01_Searching/BinarySearch/Problems/SplitArray.java
@@ -1,53 +1,52 @@
 //  https://leetcode.com/problems/split-array-largest-sum/
-
-
 import java.util.*;
 
 public class Main
 {
 	public static void main(String[] args) {
-	    Scanner in=new Scanner(System.in);
+	    Scanner in = new Scanner(System.in);
 	    int n=in.nextInt();
-	    int[] arr=new int[n];
-	    for(int i=0;i<n;i++){
-	        arr[i]=in.nextInt();
-	    }
+	    int[] arr = new int[n];
+	    for (int i=0;i<n;i++){
+	        	arr[i]=in.nextInt();
+		}
 	    int m=in.nextInt();
-	    System.out.println(splitArray(arr,m));
+		System.out.println(splitArray(arr,m));
 	}
 	
-	public static int splitArray(int[] arr,int m){
-	    int min=arr[0],max=0;
-	    for(int i=0;i<arr.length;i++){
-	        if(min>arr[i]){
-	            min=arr[i];
-	        }
-	        max=max+arr[i];
-	    }
+	static int splitArray(int[] arr,int m){
+	    int start=-1;
+	    int end=0;
 	    
-        int mid;
-	    while(min<max){
-	        mid=(min+(max-min))/2;
-	        
-	        int pieces=1;
-	        int sum=0;
-
-	        for(int num:arr){
-	            if(sum+num>mid){
-	                sum=num;
+	    for (int num : arr){
+	        if(num > start){
+	            start = num;
+	        }
+	        end = end + num;
+	    }
+	    while(start < end){
+	        int mid = (start + end) / 2;
+	        int sum = 0;
+	        int pieces = 1;
+	        for(int num : arr){
+	            if(sum + num > mid){
+	                sum = num;
 	                pieces++;
-	            }else{
-	                sum+=num;
 	            }
+	            else{
+	                sum += num;
+	            }
+	        
 	        }
 	        
 	        
-	        if(pieces>m){
-	            min=mid+1;
+	        if (pieces > m){
+	            start = mid+1;
 	        }else{
-	            max=mid-1;
+	            end = mid;
 	        }
 	    }
-	    return max;
+	    return end;
 	}
+	
 }
