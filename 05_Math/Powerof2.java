0584d4ebdcd17a13fb57f3d0c93972f151eed315
Poojitha-0508 <attilipooja@gmail.com>
2025-08-17 20:19:57 +0530

    Create Powerof2.java

---
+++ 05_Math/Powerof2.java
@@ -0,0 +1,15 @@
+// FIND A NUMBER IS POWER OF 2 OR NOT
+
+
+public class Main
+{
+	public static void main(String[] args) {
+	    int n=4;
+	    if (n&(n-1)==0){
+	        System.out.println ("Yes,It is power of 2");
+	    }
+	    else{
+	        System.out.println ("Yes,It is power of 2");
+	    }
+	}
+}
