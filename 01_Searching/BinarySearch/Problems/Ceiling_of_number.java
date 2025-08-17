c671c45cd9fc35699c5b27b6bb2eaaaca610d559
Poojitha-0508 <attilipooja@gmail.com>
2025-08-17 22:19:54 +0530

    Create Ceiling_of_number.java

---
+++ 01_Searching/BinarySearch/Problems/Ceiling_of_number.java
@@ -0,0 +1,33 @@
+// SEARCH FOR AN ELEMENT IN THE ARRAY (ASCENDING) USING BINARY SEARCH
+
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner in = new Scanner(System.in);
+	    int n=in.nextInt();
+	    int[] arr = new int[n];
+	    for (int i=0;i<n;i++){
+	        	arr[i]=in.nextInt();
+		}
+		int key=in.nextInt();
+	    System.out.println(BinarySearch(arr,key));
+	}
+	static int BinarySearch(int[] arr,int key){
+		int start=0,end=arr.length-1;
+
+		while(start<=end){
+			int mid=(start+end)/2;
+			if(arr[mid]==key){
+				return arr[mid];
+			}
+            else if (arr[mid]>key){
+			    end=mid-1;
+            }
+            else{
+                start=mid+1;
+            }
+		}
+		return arr[start];
+	}
+}
