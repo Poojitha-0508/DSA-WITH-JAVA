a7e7a77972480fc713a316d2ad262689db39b302
Poojitha-0508 <attilipooja@gmail.com>
2025-08-17 11:36:08 +0530

    Create Pattern_25.java

---
+++ 03_Patterns/Pattern_25.java
@@ -0,0 +1,16 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    int n=5;
+	    for(int i=1;i<=n;i++){
+	        for(int j=1;j<=(2*n)-1;j++){
+	            if(i+j==n+1 || i+j==2*n || (j>n && i==1) || (j<n && i==n)){
+	                System.out.print("*");
+	            }else{
+	                System.out.print(" ");
+	            }
+	        }
+	        System.out.println ();
+	    }
+	}
+}
