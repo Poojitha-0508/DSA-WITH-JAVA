d91e3088d41279846c26de0efb7e126f5449130f
Poojitha-0508 <attilipooja@gmail.com>
2025-08-17 21:30:51 +0530

    Create FindXOR.java

---
+++ 05_Math/FindXOR.java
@@ -0,0 +1,22 @@
+// Find XOR 0 to n
+
+
+public class Main
+{
+	public static void main(String[] args) {
+	    int n=8;
+	    System.out.println (findXOR(n));
+	}
+	static int findXOR(int n){
+	    if(n%4==0){
+	        return n;
+	    }
+	    else if(n%4==1){
+	        return 1;
+	    }else if(n%4==2){
+	        return n+1;
+	    }else{
+	        return 0;
+	    }
+	}
+}
