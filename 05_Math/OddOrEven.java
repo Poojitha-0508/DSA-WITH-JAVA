737f2aa0ab5f5aa6e7604209e19ea0c565bdc3fc
Poojitha-0508 <attilipooja@gmail.com>
2025-08-17 18:28:09 +0530

    Create OddOrEven.java

---
+++ 05_Math/OddOrEven.java
@@ -0,0 +1,12 @@
+
+public class Main
+{
+	public static void main(String[] args) {
+	    int n=68;
+	    if((n & 1)==0){
+	        System.out.println ("Number is EVEN");
+	    }else {
+	        System.out.println ("Number is ODD");
+	    }
+	}
+}
