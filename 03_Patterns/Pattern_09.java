c99110bf0eab720a913c5697fad6dbe977c31cc2
Poojitha-0508 <attilipooja@gmail.com>
2025-09-15 09:41:55 +0530

    Create Pattern_09.java

---
+++ 03_Patterns/Pattern_09.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner in =new Scanner(System.in);
+		int n=in.nextInt();
+        for(int i=1;i<=n;i++){  // n rows
+            for (int j=1;j<=(2*n)-1;j++){  //2n-1 cols 
+                if(i==1){
+                    System.out.print("*");
+                }
+                else if((i<=j) && (j<=((2*n)-i))){
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
