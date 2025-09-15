acc403c3b2e99221f52d6eaf5ff07dcba77d81f3
Poojitha-0508 <attilipooja@gmail.com>
2025-09-15 09:46:44 +0530

    Create Pattern_11.java

---
+++ 03_Patterns/Pattern_11.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner in =new Scanner(System.in);
+		int n=in.nextInt();
+        for(int i=1;i<=n;i++){  // n rows
+            for (int j=1;j<=(2*n)-1;j++){  //2n-1 cols 
+                if((i+j)%2!=0){
+                    System.out.print(" ");
+                }
+                else if(i==1){
+                    System.out.print("*");
+                }
+                else if((i<=j) && (j<=((2*n)-i))){
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
